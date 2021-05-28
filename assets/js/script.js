b198083ca4dc20b0a59b9d516d3f67b012cd317c
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-28 15:54:35 -0500

    Work on Movie Detail API Call

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,13 @@ let drinkDiv = $("#displayDrink");
 
 
 //  Place Movie Data On Page
-function renderMovieData(data) {
+function renderMovieData(data, genreIDs) {
     let cardSection = $("#card-section");
-    console.log(cardSection);
+    //console.log(cardSection);
     console.log(data);
+    //console.log(genreIDs);
 
-    $("#card-section").html("<p class=\"subtitle is-3\">Your recommendations</p>");
+    $("#card-section").html("<p class=\"subtitle is-3\">Your Recommendations</p>");
 
     let baseImageURL = "https://image.tmdb.org/t/p/w92";
 
@@ -27,14 +28,51 @@ function renderMovieData(data) {
             ],
     */
 
-    currentPage = data.page;
+    //  Set pages for Back and Next Buttons
+    let currentPage = data.page;
     let totalPages = data.total_pages;
 
-    $("#card-section").append("<p>Page: " + currentPage + " of " + totalPages + "</p>");
+    let pageUpValue = currentPage + 1;
+    let pageDownValue = currentPage - 1;
 
-    //  LOOP REQUIRED
+    //  Make sure Next Button value does not exceed total number of pages
+    if (currentPage === data.total_pages) {
+        pageUpValue = data.total_pages;
+    }
+
+    //  Make sure Back Button value does not drop below 1
+    if (currentPage === 1) {
+        pageDownValue = 1;
+    }
+
+    //  Create Back and Next buttons
+    let pageUpButton = $("<button></button>");
+    pageUpButton.attr("id", "pageUpButton");
+    pageUpButton.attr("value", pageUpValue);
+    pageUpButton.attr("class", "button is-link is-rounded is-flex-wrap-nowrap");
+    pageUpButton.append("Next");
+
+    let pageDownButton = $("<button></button>");
+    pageDownButton.attr("id", "pageDownButton");
+    pageDownButton.attr("value", pageDownValue);
+    pageDownButton.attr("class", "button is-link is-rounded is-flex-wrap-nowrap");
+    pageDownButton.append("Back");
+
+    //  Page 1 of X display
+    let pageDisplay = $("<p></p>");
+    pageDisplay.text("Page: " + currentPage + " of " + totalPages + " ");
+
+    //  Add Buttons to card-content div
+    pageDownButton.appendTo(pageDisplay);
+    pageUpButton.appendTo(pageDisplay);
+
+    pageDisplay.appendTo(cardSection);
+
+
+    //  Loop through API movie results
     for (i = 0; i < data.results.length; i++) {
 
+        //  Create div media-content for movie
         let cardDiv = $("<div></div>");
         cardDiv.attr("class", "card mb-2");
 
@@ -58,25 +96,33 @@ function renderMovieData(data) {
         let mediaRightDiv = $("<div></div>");
         mediaRightDiv.attr("class", "media-content");
 
+        //  Create save movie button
         let saveButton = $("<button></button>");
         saveButton.attr("class", "button is-link is-rounded is-flex-wrap-nowrap");
+        //  Steve - I think we need to add the JSON string to send to local storage here.
         saveButton.attr("value", data.results[i].id);
         saveButton.append("Save");
 
         let mediaTitle = $("<p></p>");
         mediaTitle.attr("class", "title is-4 is-flex is-justify-content-space-between");
-        mediaTitle.append(data.results[i].title);
+        //mediaTitle.append(data.results[i].title);
+        //mediaTitle.append(" ::: id: " + data.results[i].id);
 
         let mediaTitleLink = $("<a></a>");
-        mediaTitleLink.attr("href", "");
-        mediaTitleLink.attr("id", "movieDetails");
+        mediaTitleLink.attr("id", "movieDetails" + i);
+        //mediaTitleLink.attr("class", "title is-4 is-flex is-justify-content-space-between");
+        mediaTitleLink.attr("href", "#");
+        mediaTitleLink.attr("value", data.results[i].id);
+        mediaTitleLink.append(data.results[i].title);
+        mediaTitleLink.append(" ::: id: " + data.results[i].id);
+        mediaTitleLink.appendTo(mediaTitle);
 
         let mediaContent = $("<p></p>");
         mediaContent.attr("class", "content is-6");
         mediaContent.append(data.results[i].overview);
 
 
-        //  Place Movie Elements
+        //  Place movie elements to card-content div
         $(movieImage).appendTo(figureImage);
         $(figureImage).appendTo(mediaLeftDiv);
 
@@ -90,16 +136,49 @@ function renderMovieData(data) {
         $(mediaDiv).appendTo(cardDiv);
         cardSection.append(cardDiv);
     }
+
+    //  Event listeners for back and next buttons
+    $("#pageUpButton").click(function () {
+        console.log($(this).val());
+        movies = getAPIMovieData(genreIDs, $(this).val());
+    });
+
+    $("#pageDownButton").click(function () {
+        console.log($(this).val());
+        movies = getAPIMovieData(genreIDs, $(this).val());
+    });
+
+    //  This does not work right now - Jeff
+    $("#movieDetails").click(function () {
+        console.log($(this).val());
+        getAPIMovieDetails($(this).val()) 
+    });
+
 }
 
 
 
+//  Movie Detail Display
+function renderMovieDetails(data) {
+    let cardSection = $("#card-section");
+    //console.log(cardSection);
+    console.log(data);
+    //console.log(genreIDs);
+
+    $("#card-section").html("<p class=\"subtitle is-3\">Your Recommendations</p>");
+
+    let baseImageURL = "https://image.tmdb.org/t/p/w92";
+
+    cardSection.append(cardDiv);
+}
+
+
 //  Drink Modal
-function renderDrink(data){
+function renderDrink(data) {
     console.log(data);
 
     //  Get a new drink if over 6 Ingredients
-    if ( data.drinks[0].strIngredient7) {
+    if (data.drinks[0].strIngredient7) {
         getAPIDrink();
     }
 
@@ -115,7 +194,7 @@ function renderDrink(data){
     } else {
         drinkImage.attr("src", "");
     }
-    
+
     let drinkDetails = $("<p></p>");
     drinkDetails.append("Instructions: <br />");
     drinkDetails.append(data.drinks[0].strInstructions + "<br /><br />");
@@ -124,26 +203,56 @@ function renderDrink(data){
     //  Get Drink Ingredients and 
     let drinkIngredients = $("<p></p>");
     drinkIngredients.append("Ingredients: <br />");
-    if ( data.drinks[0].strIngredient1 ) {
-        drinkIngredients.append("1) " + data.drinks[0].strMeasure1 + ": " + data.drinks[0].strIngredient1 + "<br />");
+    if (data.drinks[0].strIngredient1) {
+        if (data.drinks[0].strMeasure1) {
+            drinkMeasure = data.drinks[0].strMeasure1 + ": ";
+        } else {
+            drinkMeasure = "";
+        }
+        drinkIngredients.append("1) " + drinkMeasure + data.drinks[0].strIngredient1 + "<br />");
     }
-    if ( data.drinks[0].strIngredient2 ) {
-        drinkIngredients.append("2) " + data.drinks[0].strMeasure2 + ": " + data.drinks[0].strIngredient2 + "<br />");
+    if (data.drinks[0].strIngredient2) {
+        if (data.drinks[0].strMeasure2) {
+            drinkMeasure = data.drinks[0].strMeasure2 + ": ";
+        } else {
+            drinkMeasure = "";
+        }
+        drinkIngredients.append("2) " + drinkMeasure + data.drinks[0].strIngredient2 + "<br />");
     }
-    if ( data.drinks[0].strIngredient3 ) {
-        drinkIngredients.append("3) " + data.drinks[0].strMeasure3 + ": " + data.drinks[0].strIngredient3 + "<br />");
+    if (data.drinks[0].strIngredient3) {
+        if (data.drinks[0].strMeasure3) {
+            drinkMeasure = data.drinks[0].strMeasure3 + ": ";
+        } else {
+            drinkMeasure = "";
+        }
+        drinkIngredients.append("3) " + drinkMeasure + data.drinks[0].strIngredient3 + "<br />");
     }
-    if ( data.drinks[0].strIngredient4 ) {
-        drinkIngredients.append("4) " + data.drinks[0].strMeasure4 + ": " + data.drinks[0].strIngredient4 + "<br />");
+    if (data.drinks[0].strIngredient4) {
+        if (data.drinks[0].strMeasure4) {
+            drinkMeasure = data.drinks[0].strMeasure4 + ": ";
+        } else {
+            drinkMeasure = "";
+        }
+        drinkIngredients.append("4) " + drinkMeasure + data.drinks[0].strIngredient4 + "<br />");
     }
-    if ( data.drinks[0].strIngredient5 ) {
-        drinkIngredients.append("5) " + data.drinks[0].strMeasure5 + ": " + data.drinks[0].strIngredient5 + "<br />");
+    if (data.drinks[0].strIngredient5) {
+        if (data.drinks[0].strMeasure5) {
+            drinkMeasure = data.drinks[0].strMeasure5 + ": ";
+        } else {
+            drinkMeasure = "";
+        }
+        drinkIngredients.append("5) " + drinkMeasure + data.drinks[0].strIngredient5 + "<br />");
     }
-    if ( data.drinks[0].strIngredient6 ) {
-        drinkIngredients.append("6) " + data.drinks[0].strMeasure6 + ": " + data.drinks[0].strIngredient6 + "<br />");
+    if (data.drinks[0].strIngredient6) {
+        if (data.drinks[0].strMeasure6) {
+            drinkMeasure = data.drinks[0].strMeasure6 + ": ";
+        } else {
+            drinkMeasure = "";
+        }
+        drinkIngredients.append("6) " + drinkMeasure + data.drinks[0].strIngredient6 + "<br />");
     }
     drinkIngredients.append("<br/>");
-    
+
     $(drinkDiv).append(drinkImage);
     $(drinkDiv).append(drinkIngredients);
     $(drinkDiv).append(drinkDetails);
@@ -160,7 +269,7 @@ function getAPIDrink() {
             console.log(response.status);
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
             renderDrink(data);
         });
 }
@@ -168,12 +277,16 @@ function getAPIDrink() {
 
 
 //  Fetch API Movie Data
-function getAPIMovieData(genreIDs, currentPage) {
-    
-    //currentPage = 5;
+function getAPIMovieData(genreIDs, newPage) {
+
+    if (newPage) {
+        currentPage = newPage;
+    } else {
+        currentPage = 1;
+    }
 
     console.log("Page: " + currentPage);
-    let movieAPI = "https://api.themoviedb.org/3/discover/movie?api_key=0d37b66cfda3facaf7d62b81d68fd669&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + currentPage + "&primary_release_date.gte=2010-01-01&primary_release_date.lte=2020-12-31&vote_average.gte=6&with_genres=" + genreIDs + "";
+    let movieAPI = "https://api.themoviedb.org/3/discover/movie?api_key=0d37b66cfda3facaf7d62b81d68fd669&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + currentPage + "&primary_release_date.gte=2010-01-01&primary_release_date.lte=2020-12-31&vote_average.gte=7&with_genres=" + genreIDs + "";
     console.log(movieAPI);
     fetch(movieAPI)
         .then(function (response) {
@@ -183,32 +296,41 @@ function getAPIMovieData(genreIDs, currentPage) {
             return response.json();
         })
         .then(function (data) {
-            renderMovieData(data);
+            renderMovieData(data, genreIDs);
         });
 }
 
 
 //  Movie Detail API 
-function getAPIMovieDetails() {
-    let movieDetailAPI = "https://developers.themoviedb.org/3/movies/get-movie-details?api_key=0d37b66cfda3facaf7d62b81d68fd669&language=en-US&"
-}
+function getAPIMovieDetails(id) {
 
-function changePage(currentPage) {
+    console.log("AT MOVE" + id);
 
+    let movieDetailAPI = "https://api.themoviedb.org/3/movie/" + id + "?api_key=0d37b66cfda3facaf7d62b81d68fd669";
+    fetch(movieDetailAPI)
+    .then(function (response){
+        if (response.status !== 200) {
+            console.log(response.status);
+        }
+        return response.json();
+    })
+    .then(function (data) {
+        renderMovieDetails(data);
+    });
+    //  "https://developers.themoviedb.org/3/getting-started/append-to-response";
 }
 
 
 
 //  Search Button Listener
 $(".genreButton").click(function () {
-    console.log($(this).val());
+    //console.log($(this).val());
     movies = getAPIMovieData($(this).val());
 });
 
 
-
 //  Drink Modal
-$( function() {
+$(function () {
     $("#displayDrink").dialog({
         modal: true,
         autoOpen: false,
