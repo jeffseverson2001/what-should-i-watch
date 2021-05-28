bc0fd8d1ccc93655a2fbd03173f65f678a30ed30
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-28 00:43:56 -0500

    Late Thurdsay Modal Learning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //  Get HTML Elements
 let buttonBlock = $(".buttons");
-
-
+let drinkDiv = $("#displayDrink");
 
 //  Place Movie Data On Page
 function renderMovieData(data) {
@@ -26,7 +25,7 @@ function renderMovieData(data) {
             ],
     */
 
-    var currentPage = data.page;
+    currentPage = data.page;
     let totalPages = data.total_pages;
 
     $("#card-section").append("<p>Page: " + currentPage + " of " + totalPages + "</p>");
@@ -39,6 +38,7 @@ function renderMovieData(data) {
 
         let mediaDiv = $("<div></div>");
         mediaDiv.attr("class", "card-content media");
+
 
         let mediaLeftDiv = $("<div></div>");
         mediaLeftDiv.attr("class", "media-left");
@@ -66,6 +66,10 @@ function renderMovieData(data) {
         mediaTitle.attr("class", "title is-4");
         mediaTitle.append(data.results[i].title);
 
+        let mediaTitleLink = $("<a></a>");
+        mediaTitleLink.attr("href", "");
+        mediaTitleLink.attr("id", "movieDetails");
+
         let mediaContent = $("<p></p>");
         mediaContent.attr("class", "content is-6");
         mediaContent.append(data.results[i].overview);
@@ -87,13 +91,69 @@ function renderMovieData(data) {
     }
 }
 
+function renderDrink(data){
+    console.log(data);
+
+    //  Jeff Notes For Modal
+
+    //  Clear Current Drink
+    $(drinkDiv).text("");
+
+    //let drinkName = $("<div></div>");
+    //drinkName.attr("id", "displayDrink");
+    //drinkName.attr("style", "display:none;");
+    //drinkName.attr("role", "dialog");
+    drinkDiv.attr("title", "How To Make A " + data.drinks[0].strDrink);
+
+    let drinkImage = $("<img />");
+    drinkImage.attr("style", "width: 25%;");
+    if (data.drinks[0].strDrinkThumb) {
+        drinkImage.attr("src", data.drinks[0].strDrinkThumb);
+    } else {
+        drinkImage.attr("src", "");
+    }
+    
+    let drinkDetails = $("<p></p>");
+    //drinkDetails.attr("class", "");
+    drinkDetails.append(data.drinks[0].strInstructions);
+
+    //drinkName.append(data.drinks[0].strDrink);
+
+    //drinkImage.appendTo(drinkName);
+    //drinkDetails.appendTo(drinkName);
+    
+    $(drinkDiv).text(drinkImage);
+    //drinkDiv.find('.modal-title').text("How To Make A " + data.drinks[0].strDrink);
+    //$(drinkDiv).text("title", "How To Make A " + data.drinks[0].strDrink);
+    $(drinkDiv).text(data.drinks[0].strInstructions);
+
+    //drinkDiv.append(drinkName);
+}
+
+
+
+function getAPIDrink() {
+    let drinkAPI = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+    fetch(drinkAPI)
+        .then(function (response) {
+            console.log(response.status);
+            return response.json();
+        })
+        .then(function(data) {
+            renderDrink(data);
+        });
+}
+
+
+
 //  Fetch API Movie Data
 function getAPIMovieData(genreIDs, currentPage) {
-    console.log(currentPage);
-    if (currentPage === "") {
-        currentPage = 1;
-    }
-    let movieAPI = "https://api.themoviedb.org/3/discover/movie?api_key=0d37b66cfda3facaf7d62b81d68fd669&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + currentPage + "&primary_release_date.gte=2000-01-01&primary_release_date.lte=2020-12-31&vote_average.gte=6&with_genres=" + genreIDs + "";
+    
+    //currentPage = 5;
+
+    console.log("Page: " + currentPage);
+    let movieAPI = "https://api.themoviedb.org/3/discover/movie?api_key=0d37b66cfda3facaf7d62b81d68fd669&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + currentPage + "&primary_release_date.gte=2010-01-01&primary_release_date.lte=2020-12-31&vote_average.gte=6&with_genres=" + genreIDs + "";
     console.log(movieAPI);
     fetch(movieAPI)
         .then(function (response) {
@@ -116,8 +176,6 @@ function changePage(currentPage) {
 }
 
 
-
-
 //  Search Button Listener
 $(".genreButton").click(function () {
     console.log($(this).val());
@@ -125,5 +183,24 @@ $(".genreButton").click(function () {
 });
 
 
+//  Drink Modal
+$( function() {
+    $("#displayDrink").dialog({
+        modal: true,
+        autoOpen: false,
+        width: 700,
+        height: 700
+    });
+    $("#getDrinkButton").click(function () {
+        getAPIDrink();
+        $("#displayDrink").dialog("open");
+    });
+});
+
+
+//$("#getDrinkButton").click(function () {
+//    console.log($(this).val());
+//    getAPIDrink();
+//});
 
 
